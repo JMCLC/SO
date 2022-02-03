64eedfa961c90d5a9625e742237257b6b0ff3471
Pulga <pulgapt123@gmail.com>
2022-02-03 00:36:09 +0000

    Mount adicionado

--- 2/tecnicofs_ex2/client/tecnicofs_client_api.c
+++ 2/tecnicofs_ex2/client/tecnicofs_client_api.c
@@ -1,7 +1,29 @@
 #include "tecnicofs_client_api.h"
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int active_session;
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     /* TODO: Implement this */
+    printf("Achieved Mount \n");
+    mkfifo(client_pipe_path, 0640);
+    int sv_pipe_write = open(server_pipe_path, O_WRONLY);
+    char msg[41] = "1";
+    strcat(msg, client_pipe_path);
+    printf("Tried to write this: %s \n", msg);
+    ssize_t ret = write(sv_pipe_write, msg, strlen(msg) + 1);
+    printf("wrote \n");
+    close(sv_pipe_write);
+    int sv_pipe_read = open(server_pipe_path, O_RDONLY);
+    char buffer[1];
+    read(sv_pipe_read, buffer, 1);
+    active_session = atoi(buffer);
+    printf("Session id: %d \n", active_session);
     return -1;
 }
 
--- 2/tecnicofs_ex2/fs/tfs_server.c
+++ 2/tecnicofs_ex2/fs/tfs_server.c
@@ -1,4 +1,16 @@
 #include "operations.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE (41)
+#define S 1
+
+char pipes[S][40];
 
 int main(int argc, char **argv) {
 
@@ -11,6 +23,36 @@ int main(int argc, char **argv) {
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
     /* TO DO */
+    if (unlink(pipename) != 0 && errno != ENOENT) {
+        printf("Unlinking pipe \n");
+        return -1;
+    }
+
+    if (mkfifo(pipename, 0640) != 0) {
+        printf("Error creating pipe \n");
+        return -1;
+    }
+    int rx = open(pipename, O_RDONLY);
+    if (rx == -1) {
+        printf("Error opening File \n");
+        return -1;
+    }
+    int numberOfConnections = 0;
+    while (1) {
+        char buffer[BUFFER_SIZE];
+        ssize_t read_size = read(rx, buffer, BUFFER_SIZE);
+        if (buffer[0] == '1' && numberOfConnections == 0) {
+            printf("Received this: size: %ld info: %s \n", read_size, buffer);
+            for (int i = 1; i < BUFFER_SIZE; i++) {
+                pipes[numberOfConnections][i-1] = buffer[i]; 
+            }
+            numberOfConnections += 1;
+            int write_info = open(pipename, O_WRONLY);
+            ssize_t write_size = write(rx, "0", 2);
+            printf("wrote to client: %ld \n", write_size);
+            close(write_info);
+        }
+    }
 
     return 0;
 }
